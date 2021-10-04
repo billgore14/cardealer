21cb070dff22ba1c0ad112d05e1c72a6139e00f7
billgore14 <90286229+billgore14@users.noreply.github.com>
2021-10-04 16:41:13 -0400

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ def returnConnection():
     conn = connect(
         host = "mydatabasee.cphw5qnsj00k.us-east-1.rds.amazonaws.com",
         user = "admin",
-        password = "rootroot",
-        database = "mydatabaseaci"
+        password = 
+        database = 
     )
     return conn
